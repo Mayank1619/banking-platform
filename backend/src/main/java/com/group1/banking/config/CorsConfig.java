621e427820423e88d511fe719814781da9ddcbb8
Emad Altaki <s.emadaltaki@gmail.com>
2026-06-08 11:59:56 -0400

    fix: Update CORS configuration to allow all localhost subdomains and modify database lock file

--- backend/src/main/java/com/group1/banking/config/CorsConfig.java
+++ backend/src/main/java/com/group1/banking/config/CorsConfig.java
@@ -41,9 +41,8 @@ public class CorsConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of(
-                "http://localhost:5173",
-                "http://localhost:3000",
+        config.setAllowedOriginPatterns(List.of(
+                "http://localhost:*",
                 "https://frontend-524103119199.northamerica-northeast2.run.app",
                 "https://digital-banking-service-524103119199.northamerica-northeast1.run.app",
                 "http://34.72.76.94"
